5f3f3bbbf2522362abb3bad64215c788a1702a1f
Sabbir Chowdhury <sabbirchowdhury40854@gmail.com>
2025-07-11 21:16:47 +0600

    Update layout.tsx 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,14 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth">
 
          <head>
-        {/* SmartCode script */}
+      
+     
         <script
-          src="https://tigsaw.com/api/delivery/8YKZ3X05/common"
+          src="https://tigsaw.com/api/delivery/5EDJ6SZH/common"
           async
         ></script>
+      
+      </Head>
       </head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
