e48db465fcf281d7f25a7af93ba0a4cde0536822
Sabbir Chowdhury <sabbirchowdhury40854@gmail.com>
2025-07-25 02:02:06 +0600

    add tigsaw script

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,12 +22,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+  <head>
+
+<script id="tigsaw-script" tigsaw-id="LAETQP5C">
+(function(t,i,g,s,a,w){
+  i=t.currentScript.getAttribute("tigsaw-id");
+  (w||window).tigsawContainerId=i;
+  g=t.createElement('script');
+  g.src='https://static.tigsaw.com/delivery/smartscript.js';
+  g.defer=true;
+  t.head.appendChild(g);
+})(document,typeof window!=='undefined'?window:this);
+</script>
+
+
+  
+  </head>
+      
+      
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        {/* External Script */}
-        <Script src="https://tigsaw.com/api/delivery/5EDJ6SZH/common" strategy="afterInteractive" />
-
+       
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
